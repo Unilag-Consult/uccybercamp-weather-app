988bad6e04fce31682e2bb010bd8ce3098e3df8b
Hendrick <sebastinesoacatp@gmail.com>
2021-08-15 16:00:56 +0100

    Backend Done

--- lib/json2dart.dart
+++ lib/json2dart.dart
@@ -1,14 +1,18 @@
 class DailyForecast {
-  double temp;
-  int pressure;
-  int humidity;
-  double windSpeed;
-  String weather;
-  String name;
-  String country;
+  double? temp;
+  double? tempMin;
+  double? tempMax;
+  int? pressure;
+  int? humidity;
+  double? windSpeed;
+  String? weather;
+  String? name;
+  String? country;
 
   DailyForecast({
     required this.temp,
+    required this.tempMin,
+    required this.tempMax,
     required this.pressure,
     required this.humidity,
     required this.windSpeed,
@@ -20,6 +24,8 @@ class DailyForecast {
   factory DailyForecast.fromJson(Map<String, dynamic> jsonData) {
     return DailyForecast(
       temp: jsonData['main']['temp'],
+      tempMin: jsonData['main']['temp_min'],
+      tempMax: jsonData['main']['temp_max'],
       pressure: jsonData['main']['pressure'],
       humidity: jsonData['main']['humidity'],
       windSpeed: jsonData['wind']['speed'],
@@ -27,5 +33,29 @@ class DailyForecast {
       name: jsonData['name'],
       country: jsonData['sys']['country'],
     );
+  }
+
+  static initialData() {
+    return {
+      "weather": [
+        {
+          "main": "Clear",
+        }
+      ],
+      "main": {
+        "temp": 9.4,
+        "temp_min": 7.22,
+        "temp_max": 11.11,
+        "pressure": 1023,
+        "humidity": 100
+      },
+      "wind": {
+        "speed": 1.5,
+      },
+      "sys": {
+        "country": "US",
+      },
+      "name": "Mountain View",
+    };
   }
 }
